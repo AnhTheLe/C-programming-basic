cebdb72e5f97248d319f7bf1e88efa08fc053050
BN2002 <doanhcms@gmail.com>
2021-10-09 21:33:38 +0700

    doc danh ba thu 2 va thu 3 tu file

---
+++ week2/doc_danh_ba_theo_vi_tri.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct {
+	char name[50];
+	char phone[20];
+	char email[100];
+} thong_tin_lien_lac;
+
+int main()
+{
+
+	printf("Thong tin danh ba o vi tri thu 2 va vi tri thu 3 \n");
+	FILE * fin;
+	fin = fopen("danh_ba.dat", "rb");
+	if(fin == NULL)
+	{
+		printf("Loi mo file\n");
+		exit(0);
+	}
+	
+	thong_tin_lien_lac * danh_ba;
+	danh_ba = malloc(sizeof(thong_tin_lien_lac) * 2);
+	if(danh_ba == NULL)
+	{
+		printf("Loi cap phat bo nho");
+		exit(0);
+	}
+	fseek(fin, 1 * sizeof(thong_tin_lien_lac), SEEK_SET);
+	fread(danh_ba, sizeof(thong_tin_lien_lac), 2, fin);
+	for(int i = 0; i < 2; i++)
+	{
+		printf("Thong tin lien he cua nguoi thu: %d \n", i);
+		printf("\tHo ten: %s\n", danh_ba[i].name);
+		printf("\tSDT: %s\n", danh_ba[i].phone);
+		printf("\tEmail: %s\n", danh_ba[i].email);
+	}
+	
+	fclose(fin);
+	return 0;
+}
